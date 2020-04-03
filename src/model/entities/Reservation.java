44a59e9e3d8ab34a0e03cb4b23e62b7150bb2d31
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2020-04-02 21:30:31 -0300

    first solution(very bad)

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,63 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+	
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder strBuilder = new StringBuilder();
+		
+		strBuilder.append("Room ")
+				  .append(roomNumber)
+				  .append(", check-in: ")
+				  .append(sdf.format(checkIn))
+				  .append(", check-out: ")
+				  .append(sdf.format(checkOut))
+				  .append(", ")
+				  .append(duration())
+				  .append(" nights");
+		
+		return strBuilder.toString();
+	}
+}
